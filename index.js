f02c7a6d072346c665c20a676d5dc38747028441
Stebbi <stefodd@yahoo.com>
2022-04-17 17:19:36 -0500

    capture user input for manager, engineer, intern

---
+++ index.js
@@ -0,0 +1,225 @@
+const inquirer = require('inquirer');
+const Manager = require('./lib/Manager');
+const Engineer = require('./lib/Engineer');
+const Intern = require('./lib/Intern');
+const fs = require('fs');
+const path = require('path');
+const { TestResult } = require('@jest/types');
+
+const myTeam = [];
+
+const queryManager = () => {
+    return inquirer.prompt([
+        {
+            type: 'input',
+            name: 'name',
+            message: 'Please enter your name (Required)',
+            validate: nameInput => {
+                if (!nameInput) {
+                    console.log('Please enter your name!');
+                    return false;
+                }
+                return true;
+            }
+        },
+        {
+            type: 'input',
+            name: 'id',
+            message: 'Please enter your employee ID (Required)',
+            validate: idInput => {
+                if (!idInput) {
+                    console.log('Please enter a valid ID number!');
+                    return false;
+                }
+                return true;
+            }
+        },
+        {
+            type: 'input',
+            name: 'email',
+            message: 'Please enter your employee email address (Required)',
+            validate: emailInput => {
+                if (!emailInput) {
+                    console.log('Please enter your employee email address!');
+                    return false;
+                }
+                return true;
+            }
+        },
+        {
+            type: 'input',
+            name: 'officeNumber',
+            message: 'Please enter your office number (Required)',
+            validate: officeNumber => {
+                if (!officeNumber) {
+                    console.log('Please enter your office number!');
+                    return false;
+                }
+                return true;
+            }
+        },
+    ])
+    .then((answers) => {
+        console.log(answers);
+        const manager = new Manager(answers.name, answers.id, answers.email, answers.officeNumber);
+        myTeam.push(manager);
+        querySelect();
+    })
+};
+
+const querySelect = () => {
+    return inquirer.prompt([
+        {
+            type: 'list',
+            name: 'menu',
+            message: 'Please select how you wish to proceed:',
+            choices: ['Add an engineer', 'Add an intern', 'Team Complete']
+        }
+    ])
+    .then(choice => {
+        switch (choice.menu) {
+            case 'Add an engineer':
+                queryEngineer();
+                break;
+            case 'Add an intern':
+                queryIntern();
+                break;
+            default:
+                completeTeam();
+        }
+    });
+};
+
+const queryEngineer = () => {
+    console.log(`
+    ==================
+    Add a New Engineer
+    ==================
+    `);
+
+    return inquirer.prompt([
+        {
+            type: 'input',
+            name: 'name',
+            message: "Please enter engineer's name (Required)",
+            validate: nameInput => {
+                if (!nameInput) {
+                    console.log("Please enter engineer's name!");
+                    return false;
+                }
+                return true;
+            }
+        },
+        {
+            type: 'input',
+            name: 'id',
+            message: 'Please enter your employee ID (Required)',
+            validate: idInput => {
+                if (!idInput) {
+                    console.log('Please enter a valid ID number!');
+                    return false;
+                }
+                return true;
+            }
+        },
+        {
+            type: 'input',
+            name: 'email',
+            message: 'Please enter your employee email address (Required)',
+            validate: emailInput => {
+                if (!emailInput) {
+                    console.log('Please enter your employee email address!');
+                    return false;
+                }
+                return true;
+            }
+        },
+        {
+            type: 'input',
+            name: 'gitHub',
+            message: 'Please enter your GitHub username (Required)',
+            validate: gitHub => {
+                if (!gitHub) {
+                    console.log('Please enter your GitHub username!');
+                    return false;
+                }
+                return true;
+            }
+        }
+    ])
+    .then((answers) => {
+        console.log(answers);
+        const engineer = new Engineer(answers.name, answers.id, answers.email, answers.gitHub);
+        myTeam.push(engineer);
+        querySelect();
+    })
+};
+
+const queryIntern = () => {
+    console.log(`
+    ==================
+    Add a New Intern
+    ==================
+    `);
+
+    return inquirer.prompt([
+        {
+            type: 'input',
+            name: 'name',
+            message: "Please enter intern's name (Required)",
+            validate: nameInput => {
+                if (!nameInput) {
+                    console.log("Please enter engineer's name!");
+                    return false;
+                }
+                return true;
+            }
+        },
+        {
+            type: 'input',
+            name: 'id',
+            message: 'Please enter your employee ID (Required)',
+            validate: idInput => {
+                if (!idInput) {
+                    console.log('Please enter a valid ID number!');
+                    return false;
+                }
+                return true;
+            }
+        },
+        {
+            type: 'input',
+            name: 'email',
+            message: 'Please enter your employee email address (Required)',
+            validate: emailInput => {
+                if (!emailInput) {
+                    console.log('Please enter your employee email address!');
+                    return false;
+                }
+                return true;
+            }
+        },
+        {
+            type: 'input',
+            name: 'school',
+            message: 'Please enter your affiliated school name (Required)',
+            validate: school => {
+                if (!school) {
+                    console.log('Please enter your affiliated school name!');
+                    return false;
+                }
+                return true;
+            }
+        }
+    ])
+    .then((answers) => {
+        console.log(answers);
+        const intern = new Intern(answers.name, answers.id, answers.email, answers.school);
+        myTeam.push(intern);
+        querySelect();
+    })
+};
+
+
+
+queryManager();
